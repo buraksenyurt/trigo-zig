ac502422a71c1039cc63143ec160586da88884c1
Burak Selim Senyurt <burakselimsenyurt@gmail.com>
2026-03-05 11:03:34 +0300

    Refactor sine and cosine functions to use normalized angle for calculations

--- lib.zig
+++ lib.zig
@@ -1,21 +1,37 @@
 const std = @import("std");
 
 /// A simple implementation of sine and cosine functions using Taylor series expansion.
+fn normalizeAngle(a: f64) f64 {
+    const tau = std.math.tau;
+
+    var angle = @mod(a, tau);
+    if (angle > std.math.pi) {
+        angle -= tau;
+    } else if (angle < -std.math.pi) {
+        angle += tau;
+    }
+    return angle;
+}
+
 pub fn sin(value: f64) f64 {
+    const normalizedValue = normalizeAngle(value);
+
     var result: f64 = 0;
-    var term: f64 = value;
+    var term: f64 = normalizedValue;
     var n: f64 = 1;
 
     var i: usize = 0;
     while (i < 10) : (i += 1) {
         result += term;
         n += 2.0;
-        term *= -1.0 * value * value / ((n - 1.0) * n);
+        term *= -1.0 * normalizedValue * normalizedValue / ((n - 1.0) * n);
     }
     return result;
 }
 
 pub fn cos(value: f64) f64 {
+    const normalizedValue = normalizeAngle(value);
+
     var result: f64 = 0;
     var term: f64 = 1;
     var n: f64 = 0;
@@ -24,7 +40,7 @@ pub fn cos(value: f64) f64 {
     while (i < 10) : (i += 1) {
         result += term;
         n += 2.0;
-        term *= -1.0 * value * value / ((n - 1.0) * n);
+        term *= -1.0 * normalizedValue * normalizedValue / ((n - 1.0) * n);
     }
     return result;
 }
